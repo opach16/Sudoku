67e66ffbe700e9fedf8e34ad61eeae9c46d2aac0
Konrad Opach <opach16@outlook.com>
2024-09-23 16:14:14 +0200

    Modified the possibleValue List implementation to allow value deletion

--- src/main/java/com/kodilla/sudoku/SudokuElement.java
+++ src/main/java/com/kodilla/sudoku/SudokuElement.java
@@ -1,12 +1,14 @@
 package com.kodilla.sudoku;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class SudokuElement {
 
     private int value;
     public final static int EMPTY = -1;
-    private final List<Integer> possibleValue = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+    private List<Integer> possibleValue = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
 
     public SudokuElement() {
         this.value = EMPTY;
