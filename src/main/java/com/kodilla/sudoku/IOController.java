5a11f7efc34d48d6ca69c0660cfe3f9cabfcb426
Konrad Opach <opach16@outlook.com>
2024-09-26 13:54:55 +0200

    Modified input value retrieval implementation to allow the value "SUDOKU"

--- src/main/java/com/kodilla/sudoku/IOController.java
+++ src/main/java/com/kodilla/sudoku/IOController.java
@@ -7,31 +7,39 @@ public class IOController {
 
     private final static Scanner SCANNER = new Scanner(System.in);
 
-    private int getValueFromUser() {
-        int userInput;
+    private String getValueFromUser() {
+        String userInput;
         while (true) {
             System.out.print("Your choice: ");
+            userInput = SCANNER.nextLine();
+            if (userInput.equalsIgnoreCase("SUDOKU")) {
+                return userInput;
+            }
             try {
-                userInput = SCANNER.nextInt();
-                if (userInput > 99 && userInput < 1000) {
+                int userNumericInput = Integer.parseInt(userInput);
+                if (userNumericInput > 99 && userNumericInput < 1000) {
                     break;
                 } else {
-                    System.out.println("Please enter a number between 100 and 999");
+                    System.out.println("Please enter a number between 100 and 999!");
                 }
-            } catch (InputMismatchException e) {
-                System.out.println("Please enter a number between 100 and 999");
-                SCANNER.next();
+            } catch (InputMismatchException | NumberFormatException e) {
+                System.out.println("Please enter a number between 100 and 999 or 'SUDOKU'!");
             }
         }
         return userInput;
     }
 
-    public ElementDto getElement() {
-        int userInput = getValueFromUser();
+
+    public ElementResponse getElement() {
+        String valueFromUser = getValueFromUser();
+        if (valueFromUser.equalsIgnoreCase("SUDOKU")) {
+            return new ElementResponse(true, null);
+        }
+        int userInput = Integer.parseInt(valueFromUser);
         int row = (userInput / 100) % 10;
         int col = (userInput / 10) % 10;
         int value = userInput % 10;
-        return new ElementDto(row, col, value);
+        return new ElementResponse(false, new ElementDto(row, col, value));
     }
 
     public boolean playAgain() {
@@ -40,9 +48,9 @@ public class IOController {
             try {
                 String userInput = SCANNER.nextLine();
                 if (userInput.equalsIgnoreCase("X")) {
-                    System.exit(1);
+                    System.exit(0);
                 } else if (userInput.isEmpty()) {
-                    return true;
+                    return false;
                 }
             } catch (InputMismatchException ignored) {
                 System.out.println("Please press enter or 'X' to exit ");
